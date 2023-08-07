bb58709fdc508c5ddf4a32a5494d2e866d26b374
Amjad Khaliliah <amjadkhaliliah1998@gmail.com>
2023-08-07 18:02:02 +0300

    Create UploadFile

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -3,16 +3,16 @@ package Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@org.springframework.stereotype.Controller
 @RequestMapping ("/SparkyWeb")
 public class Controller {
 
 
     @GetMapping("/UploadFile")
     public String UploadFile(){
-        return "index";
+        return "template";
     }
 
 
